5c7b80dedbaf806e118324c80b3248ded642d093
brandon <brandonmogy@gmail.com>
2023-11-17 15:26:49 -0800

    finished delete a song api

--- api/main.py
+++ api/main.py
@@ -3,8 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 from routers import users
 from routers import songs
-from routers import songs
-from routers import users
+
 
 app = FastAPI()
 app.include_router(users.router)
