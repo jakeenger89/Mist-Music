921222558fe060d95e1ac275cf4e9bfa3f5ba77b
KiD MOGS <brandonmogy@gmail.com>
2023-11-28 01:45:24 +0000

    Frontend songs

---
+++ ghi/src/Nav.js
@@ -0,0 +1,14 @@
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">MistMusic</NavLink>
+                <NavLink className="navbar-brand" to="/loginform">Login</NavLink>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
