ea2fb12c71ff5b8455c8567c30dbbf2dfc3ef347
Matthew Lee <matthew.lee972@gmail.com>
2023-11-29 12:47:13 -0800

    ran black on main.py

--- api/main.py
+++ api/main.py
@@ -9,9 +9,7 @@ app.include_router(authenticator.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -26,7 +24,7 @@ def launch_details():
             "week": 17,
             "day": 5,
             "hour": 19,
-            "min": "00"
+            "min": "00",
         }
     }
 
