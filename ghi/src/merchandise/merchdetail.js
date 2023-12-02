3313ae841a88d90249e4683d7b88ec0c2865fdd4
Marcus <masaoware@gmail.com>
2023-12-01 17:07:11 -0800

    Fixing front-end-job errors.

--- ghi/src/merchandise/merchdetail.js
+++ ghi/src/merchandise/merchdetail.js
@@ -30,7 +30,7 @@ function OrderForm() {
         }
         const response = await fetch(orderURL, fetchOptions)
         if(response.ok) {
-            const newOrder = await response.json();
+            // const newOrder = await response.json();
             setFirstName('')
             setLastName('')
             setAddress('')
