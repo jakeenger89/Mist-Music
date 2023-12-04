a3a4df7bedbab5b1203b4e35fbb572078bf08749
brandon <brandonmogy@gmail.com>
2023-12-04 09:22:49 -0800

    testing

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
+import './indexgit .css';
 import Nav from "./Nav";
 import AccountForm from "./LoginForm";
 import CreateSongForm from "./CreateSongForm";
