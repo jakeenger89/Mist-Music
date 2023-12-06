bb5c6d62563cacf2bf89785d768e81a851aa9b12
Brandon Moghadam <brandonmogy@gmail.com>
2023-12-06 17:45:40 +0000

    Journal

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -14,6 +14,7 @@ import AllAccountSongs from "./allAccountSongs";
 import UpdateSongForm from "./updateSongForm";
 import AboutUs from "./aboutUs";
 import UserLikedSongs from "./UserLikedSongs";
+import Profile from "./profilepage"
 
 
 function App() {
