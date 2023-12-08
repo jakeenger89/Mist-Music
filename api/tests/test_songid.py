de2ee50b0dc24913c2cf3ac58e9a7a4e9e83de9d
Brandon Moghadam <brandonmogy@gmail.com>
2023-12-08 19:47:36 +0000

    Unit Test

---
+++ api/tests/test_songid.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+class TestSongQueries:
+    def test_get_song_by_id(self):
+
+        song_id = 1
+
+        response = client.get(f"/api/songs/{song_id}")
+
+        assert response.status_code == 200
--- ghi/src/edit.js
+++ ghi/src/edit.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
-const EditAccount = ({}) => {
+const EditAccount = () => {
     const [username, setUsername] = useState("")
     const [first_name, setFirstName] = useState("")
     const [last_name, setLastName] = useState("")
